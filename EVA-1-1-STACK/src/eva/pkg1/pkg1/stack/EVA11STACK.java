135c180bb5a7c3c447a33832b6a9366ced92cc3e
lisaddest <104457963+lisaddest@users.noreply.github.com>
2022-08-19 09:50:31 -0600

    Add files via upload

--- EVA-1-1-STACK/src/eva/pkg1/pkg1/stack/EVA11STACK.java
+++ EVA-1-1-STACK/src/eva/pkg1/pkg1/stack/EVA11STACK.java
@@ -11,21 +11,30 @@ package eva.pkg1.pkg1.stack;
  */
 public class EVA11STACK {
 
-    /**
+     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("INICIA MAIN");
        A();
+      
+        System.out.println("RETOMAR EJECUCIÓN MAIN");
+        System.out.println("TERMINA MAIN");
     }
  
     public static void A() {
+        System.out.println("INICIA A");
         B();
+        System.out.println("RETOMA EJECUCIÓN A");
+        System.out.println("TERMINA A");
     }
     
     public static void B() {
+        System.out.println("INICIA B");
         int i=1;
         double d= 10.0;
+        System.out.println("TERMINA B");
         
     }
 }
