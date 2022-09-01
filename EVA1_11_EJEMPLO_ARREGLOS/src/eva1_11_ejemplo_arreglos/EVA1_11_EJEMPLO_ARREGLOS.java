327f3034adfa2b84ac6537ed696b8714b31bf222
lisaddest <104457963+lisaddest@users.noreply.github.com>
2022-09-01 09:48:56 -0600

    Add files via upload

---
+++ EVA1_11_EJEMPLO_ARREGLOS/src/eva1_11_ejemplo_arreglos/EVA1_11_EJEMPLO_ARREGLOS.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_11_ejemplo_arreglos;
+
+import java.util.Scanner;
+import sun.java2d.loops.CompositeType;
+
+/**
+ *
+ * @author Ana Paulina Villegas
+ */
+public class EVA1_11_EJEMPLO_ARREGLOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*Pedir no. de grupo
+            pedir no. de estudiantes por grupo
+            capturar calificaciones
+        imprimir calificaciones
+        */
+        
+        int arregloCali[][];
+        //primer dimension --> grupos
+        //segunda dimension --> estudiantes x grupo
+        //Determinar los grupos
+        Scanner input = new Scanner(System.in);
+        System.out.println("Introduce el numero de grupos: ");
+        int iGpos = input.nextInt();
+        arregloCali = new int[iGpos][];
+        
+        
+        //para cada gpo: cuantos estudiantes necesitas?
+        for (int i = 0; i < arregloCali.length; i++) {
+            System.out.println("Cuantos estudiantes hay en el grupo " + (i+1));
+            int iEstu = input.nextInt();
+           arregloCali[i] =new int[iEstu]; 
+           
+        }
+        
+        
+        
+       
+       
+       }
+    
+        
+    }
+    
+
