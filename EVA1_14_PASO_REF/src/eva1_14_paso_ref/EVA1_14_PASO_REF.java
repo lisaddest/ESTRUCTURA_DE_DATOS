69dd790f94df6eb10e6288fea744c511bcf8a41c
lisaddest <104457963+lisaddest@users.noreply.github.com>
2022-09-02 09:57:07 -0600

    Add files via upload

---
+++ EVA1_14_PASO_REF/src/eva1_14_paso_ref/EVA1_14_PASO_REF.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_14_paso_ref;
+
+/**
+ *
+ * @author Ana Pauliina Villegas
+ */
+public class EVA1_14_PASO_REF {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] arregloDatos = new int[100];
+        llenar(arregloDatos);
+        imprimir(arregloDatos);
+    }
+    public static void llenar(int[] arreglo){
+        for (int i = 0; i < arreglo.length; i++) {
+            arreglo[i] =100;
+            
+        }
+    }
+     public static void imprimir(int[] arreglo){
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.println("[" + arreglo[i] + "]");
+    
+}
+     
+     }
+}
