ba0117ce681cd59581e665cfe815d38194f517a1
lisaddest <104457963+lisaddest@users.noreply.github.com>
2022-09-08 09:13:26 -0600

    Update EVA1_15_COPIA_ARREGLOS.java

--- EVA1_15_COPIA_ARREGLOS/src/eva1_15_copia_arreglos/EVA1_15_COPIA_ARREGLOS.java
+++ EVA1_15_COPIA_ARREGLOS/src/eva1_15_copia_arreglos/EVA1_15_COPIA_ARREGLOS.java
@@ -15,26 +15,29 @@ public class EVA1_15_COPIA_ARREGLOS {
      * @param args the command line arguments
      */
    public static void main(String[] args) {
-        // TODO code application logic here
+          // TODO code application logic here
         int[] arregloDatos = new int[100];
         llenar(arregloDatos);
         imprimir(arregloDatos);
         //COPIAR LOS VALORES ALMACENADOS EN EL ARREGLO
         
         int[] arregloCopia = new int [arregloDatos.length];
-        System.out.println("");
-       System.out.println(arregloDatos);
-       System.out.println(arregloCopia);
+        System.out.print("");
+       System.out.print(arregloDatos);
+       System.out.print(arregloCopia);
        //como crear la copia de los valores??
+       //int cont = arregloDatos.length -1;
        for (int i = 0; i < arregloDatos.length; i++) {
-            arregloCopia[i] = arregloDatos[i];
+            arregloCopia[i] = arregloDatos[(arregloDatos.length -1) - i];
+            
+
+            //cont--;
         }
        
         //imprimir
         imprimir(arregloCopia);
-         for (int i = 0; i < arregloDatos.length; i--) {
-            arregloCopia[i] = arregloDatos[i];
-        }
+         
+        
         
     }
     public static void llenar(int[] arreglo){
@@ -46,9 +49,8 @@ public class EVA1_15_COPIA_ARREGLOS {
     
      public static void imprimir(int[] arreglo){
         for (int i = 0; i < arreglo.length; i++) {
-            System.out.println("[" + arreglo[i] + "]");
+            System.out.print("[" + arreglo[i] + "]");
     
 }
-     
      }
 }
